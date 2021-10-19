c307044766d4693eb055ba6623e635e349efa264
Danilo Valim <danilo.valim@guide.com.br>
2021-10-19 10:31:52 -0300

    Added repository

---
+++ lib/app/home_controller.dart
@@ -0,0 +1,13 @@
+import 'package:core_app/core_app.dart';
+
+class HomeController {
+  late ApiRepository _repository;
+
+  HomeController() {
+    _repository = ApiRepository();
+  }
+
+  getPosts() {
+    _repository.getPosts();
+  }
+}
--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_module/app/home_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,14 +9,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  HomeController controller = HomeController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Module"),
       ),
-      body: Center(
-        child: Text("Home Module"),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Home Module"),
+            TextButton(
+              onPressed: controller.getPosts,
+              child: Text("Get Posts"),
+            ),
+          ],
+        ),
       ),
     );
   }
